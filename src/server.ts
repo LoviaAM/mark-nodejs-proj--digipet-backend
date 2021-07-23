133a5f8c0cef623b06d6e85ed510d73c168905ce
Lovia AM <lsjmicah@gmail.com>
2021-07-23 16:33:09 +0100

    deployment task

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { getDigipet } from "./digipet/model";
-import { hatchDigipet, walkDigipet, trainDigipet,ignoreDigipet } from "./digipet/controller";
+import { hatchDigipet, walkDigipet, trainDigipet,ignoreDigipet, feedDigipet } from "./digipet/controller";
 
 const app = express();
 
@@ -94,6 +94,22 @@ app.get("/digipet/ignore", (req, res) => {
     ignoreDigipet();
     res.json({
       message: "You have ignored ",
+      digipet: getDigipet(),
+    });
+  } else {
+    res.json({
+      message:
+        "You don't have a digipet. Try hatching one with /digipet/hatch",
+    });
+  }
+});
+
+app.get("/digipet/feed", (req, res) => {
+  // check the user has a digipet to walk
+  if (getDigipet()) {
+    feedDigipet();
+    res.json({
+      message: "You have fed your digipet",
       digipet: getDigipet(),
     });
   } else {
